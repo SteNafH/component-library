32cc11392382bf9d3828856888bbbf6a2a8654ef
SteNafH <stefanh2005@gmail.com>
2024-02-04 17:22:54 +0100

    fix: try and trigger publish

--- packages/module1/src/hello-world/HelloWorld.tsx
+++ packages/module1/src/hello-world/HelloWorld.tsx
@@ -4,10 +4,10 @@ export function HelloWorld() {
     const viewport = useViewport();
 
     if (viewport === 'web-desktop') {
-        return <h1>Hello World - desktop</h1>;
+        return <h1>Hello World! - desktop</h1>;
     }
 
-    return <h1>Hello World - mobile</h1>;
+    return <h1>Hello World! - mobile</h1>;
 }
 
 export default HelloWorld;
